10e53560ee13ff8212465f287b0c617103aed6d4
miha21v@mail.ru <miha21v@mail.ru>
2023-02-28 11:33:50 +0300

    Черновая вёрстка основных экранов

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from kivy_garden.mapview import MapView
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 Window.size = (375, 750)
+
+class SearchScreen(Screen):
+    pass
+
+class ProfileScreen(Screen):
+    pass
 
 class HomeScreen(Screen):
     pass
@@ -10,10 +18,12 @@ class HomeScreen(Screen):
 class MainApp(MDApp):
 
     def build(self):
-        return HomeScreen()
+        return Builder.load_file('markup_files/main.kv')
 
-    def on_start(self):
-        pass
+    def change_screen(self, screen, direction):
+        screen_manager = self.root.ids.screen_manager
+        screen_manager.current = screen
+        screen_manager.transition.direction = direction
 
 if __name__ == "__main__":
     MainApp().run()
